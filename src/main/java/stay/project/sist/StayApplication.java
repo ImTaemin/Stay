67f198013047009e49e3d0c9ca185365083d70fe
ImTaemin <kw3135@gmail.com>
2021-12-23 12:44:04 +0900

    몽고 포기 / 설정 되돌려놓음

--- src/main/java/stay/project/sist/StayApplication.java
+++ src/main/java/stay/project/sist/StayApplication.java
@@ -4,11 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @SpringBootApplication
 @ComponentScan({"stay.data.*", "stay.project.sist"})
-@EnableReactiveMongoRepositories("stay.data.*")
 @MapperScan("stay.data.*")
 public class StayApplication {
 
