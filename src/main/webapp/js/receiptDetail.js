650e2a522236c85a4c7a02969a015aace896e1c7
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-01-04 12:33:13 +0900

    22.01.04_hosting 여부 출력

---
+++ src/main/webapp/js/receiptDetail.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	// 날짜 계산
+	var startDate = $("input[name=startDate]").attr("value").split("-");
+	var endDate = $("input[name=endDate]").attr("value").split("-");
+
+	var start = new Date(startDate[0], startDate[1], startDate[2]);
+	var end = new Date(endDate[0], endDate[1], endDate[2]);
+
+	var btMs = end.getTime() - start.getTime();
+	var betweenDay = btMs / (1000 * 60 * 60 * 24);
+	
+	$("span[id=betweenDay]").html(betweenDay + "박");
+	
+	// 숙박 가격 계산
+	var roomPrice = $("input[name=roomPrice]").attr("value");
+	var calPrice = roomPrice * betweenDay;
+	var taxPrice = calPrice * 0.2;
+	
+	$("span[id=calPrice]").html("￦" + calPrice.toLocaleString());
+	$("span[id=taxPrice]").html("￦" + taxPrice.toLocaleString());
+});
