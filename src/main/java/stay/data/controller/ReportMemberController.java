47318a5475e069e138c7b23bce719ad9d3c9cdd5
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-01-05 13:48:41 +0900

    Revert "22.01.05_중간 check"
    This reverts commit 356de343ccacfa01ff892bc8c3d941bbdda81f08.

--- src/main/java/stay/data/controller/ReportMemberController.java
+++ src/main/java/stay/data/controller/ReportMemberController.java
@@ -1,5 +1,8 @@
 package stay.data.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
@@ -7,26 +10,44 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import stay.data.dto.CommentLikeDto;
+import stay.data.dto.GuestCommentDto;
+import stay.data.dto.MemberDto;
 import stay.data.dto.ReportMemberDto;
+import stay.data.dto.ResultMapDto;
+import stay.data.dto.RoomDto;
+import stay.data.dto.WishListDto;
+import stay.data.service.CommentLikeService;
+import stay.data.service.GuestCommentService;
 import stay.data.service.MemberService;
+import stay.data.service.RoomService;
+import stay.data.service.WishListService;
 
 @Controller
 public class ReportMemberController {
+	
+	
 	@Autowired
 	MemberService memberService;
-
+	
+	
 	@GetMapping("/report")
 	public ModelAndView report(HttpSession session) {
 		ModelAndView mview = new ModelAndView();
-
+		
 		List<ReportMemberDto> reportList = memberService.getReport();
-
-		mview.addObject("reportList", reportList);
-
+		
+		mview.addObject("reportList", reportList); 
+		
 		mview.setViewName("/member/memberlist");
-
+		
 		return mview;
 	}
 }
--- src/main/webapp/js/findForm.js
+++ src/main/webapp/js/findForm.js
@@ -1,63 +1,59 @@
 //핸드폰 번호 하이픈(-) 자동 입력
-$(document).on("keyup", ".phoneNumber", function() {
-	$(this).val($(this).val().replace(/[^0-9]/g, "").replace(/(^02|^0505|^1[0-9]{3}|^0[0-9]{2})([0-9]+)?([0-9]{4})$/, "$1-$2-$3").replace("--", "-"));
+$(document).on("keyup", ".phoneNumber", function() { 
+	$(this).val( $(this).val().replace(/[^0-9]/g, "").replace(/(^02|^0505|^1[0-9]{3}|^0[0-9]{2})([0-9]+)?([0-9]{4})$/,"$1-$2-$3").replace("--", "-") ); 
 });
 
-$(function() {
-	$("#searchBtn").click(function() {
-		$(".modal").fadeIn();
-	});
-
-	$(".close").click(function() {
-		$(".modal").fadeOut();
-	});
+$(function(){
+  $("#searchBtn").click(function(){
+  	$(".modal").fadeIn();
+  });
+  
+  $(".close").click(function(){
+  	$(".modal").fadeOut();
+  });
 
 });
 
 //*****아이디 찾기*****
-$(document).on('click', '#searchBtn', function(inputName, inputHp) {
+$(document).on('click','#searchBtn',function(inputName, inputHp){
 
-	var name = $('#inputName').val();
-	var hp = $('#inputHp').val();
-
-	var postdata = { 'name': name, 'hp': hp };
-
-	// 아이디 값 받고 출력하는 ajax
+  var name=$('#inputName').val();
+  var hp=$('#inputHp').val();
+  
+  var postdata={'name':name,'hp':hp};
+  
+  // 아이디 값 받고 출력하는 ajax
 	$.ajax({
-		url: "/member/findIdprocess",
-		type: "post",
-		data: postdata,
-		dataType: "json",
-		success: function(data) {
-			var findId = data.id;
-
-			if (findId == null) {
-				$('#id_user').text("");
-				$('#id_value').html("일치하는<br><br>회원정보가 없습니다");
-				$('#pwSearch_btn').hide();
+		url:"/member/findIdprocess",
+		type:"post",
+		data:postdata,
+		dataType : "json",
+		success:function(data){
+			console.log(data);
+			var findId=data.id;
+			if(findId == null){
+				$('#id_user').text("");	
+				$('#id_value').html("일치하는 회원정보가<br> 없습니다");
 			} else {
-				$('input[name=userId]').attr('value', findId);
-				
 				$('#id_user').text("회원님의 아이디는");
 				$('#id_value').text(findId);
-				$('#pwSearch_btn').show();
 			}
 		}
 	});
-});
-
-
+ });
+ 
+ 
 //*****비밀번호 찾기*****
-$(document).on('click', '#searchBtn2', function() {
+$(document).on('click','#searchBtn2',function(){
 	$.ajax({
-		url: "/member/findPw",
-		type: "POST",
+		url : "/member/findPw",
+		type : "POST",
 		dataType: "text",//반환받는 데이터타입
-		data: {
-			"id": $("#id").val(),
-			"e_mail": $("#e_mail").val()
+		data : {
+			"id" : $("#id").val(),
+			"e_mail" : $("#e_mail").val()
 		},
-		success: function(result) {
+		success : function(result) {
 			alert(result);
 		},
 	});
