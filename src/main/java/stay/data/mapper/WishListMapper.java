8791c4f87b661d645aa394ddb9545256093d1a29
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-01-05 16:55:40 +0900

    22.01.05_chat 손절

--- src/main/java/stay/data/mapper/WishListMapper.java
+++ src/main/java/stay/data/mapper/WishListMapper.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import stay.data.dto.ResultMapDto;
 import stay.data.dto.WishListDto;
 
 @Mapper
@@ -13,5 +12,4 @@ public interface WishListMapper {
 	public void insertWishList(WishListDto wishDto);
 	public void deleteWishList(HashMap<String, String> map);
 	public List<WishListDto> getWishList(String guestId);
-//	public List<ResultMapDto> getWishList(String guestId);
 }
