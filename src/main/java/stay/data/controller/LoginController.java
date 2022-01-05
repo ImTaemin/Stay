b81f0917730a3f0f85961226a161fafef058305c
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-01-05 13:51:40 +0900

    22.01.05_check

--- src/main/java/stay/data/controller/LoginController.java
+++ src/main/java/stay/data/controller/LoginController.java
@@ -5,9 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Map;
 
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +15,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -29,23 +26,17 @@ import stay.data.service.MemberService;
 @Controller
 @RequestMapping("/member")
 public class LoginController {
-
 	@Autowired
 	MemberMapper mapper;
-	
+
 	@Autowired
 	MemberService service;
-	
+
 	@Autowired
 	private KakaoLogin kakao;
-	
 
 	@GetMapping("/login")
-	public String loginForm(HttpSession session, 
-							Model model
-							//, 
-							//HttpServletRequest request
-							) {
+	public String loginForm(HttpSession session, Model model) {
 		// 아이디 얻어오기
 		String myid = (String) session.getAttribute("myid");
 
@@ -61,20 +52,14 @@ public class LoginController {
 			String name = mapper.getName(myid);
 
 			model.addAttribute("name", name);
-			
-			//이전페이지...되는지 확인할것 
-			//String referer = request.getHeader("Referer"); 
-			//return "redirect:"+ referer;
-			
+
 			return "redirect:/";
 		}
-
 	}
 
 	@PostMapping("/loginprocess")
 	public String loginProcss(@RequestParam(required = false) String cbsave, @RequestParam String id,
 			@RequestParam String pass, HttpSession session) {
-
 		HashMap<String, String> map = new HashMap<String, String>();
 
 		map.put("id", id);
@@ -82,148 +67,127 @@ public class LoginController {
 
 		int check = mapper.login(map);
 
-	      if(check == 1 && id.equals("admin")) {
-	    	 session.setAttribute("loginok", "yes");
-	    	 session.setAttribute("myid", id);
-	         return "redirect:/report";
-	      }
-		
+		if (check == 1 && id.equals("admin")) {
+			session.setAttribute("loginok", "yes");
+			session.setAttribute("myid", id);
+			return "redirect:/report";
+		}
+
 		if (check == 1) {
 			session.setAttribute("myid", id);
 			session.setAttribute("loginok", "yes");
 			session.setAttribute("saveok", cbsave);
 			session.setAttribute("mode", "guest");
-			
+
 			// 체크했을때 on, 안하면 null
 			return "redirect:/";
 		} else {
 			return "/member/passfail";
 		}
 	}
-	
+
 	@RequestMapping("/kakaologin")
 	public String kakaoLogin(@RequestParam("code") String code, HttpSession session) throws ParseException {
-		
-		//System.out.println("code : " + code);
-		
 		String access_Token = kakao.getAccessToken(code);
-        //System.out.println("controller access_token : " + access_Token);
-        
-        HashMap<String, Object> userInfo = kakao.getUserInfo(access_Token);
-        System.out.println("login Controller : " + userInfo);
 
-        String name=(String)userInfo.get("nickname");
-        String birthday=(String)userInfo.get("birthday");
-        String photo=(String)userInfo.get("profile_image");
-        String e_mail=(String)userInfo.get("email");
-        
-        Calendar date=Calendar.getInstance();
-        int nowyear=date.get(Calendar.YEAR);
-        
-        String birth=Integer.toString(nowyear)+birthday;
-        
-        //현재 날짜의 타입 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
-        //Date로 파싱
-        Date processDate = dateFormat.parse(birth);
-        //변경할 타입으로의 형 변환
-        String realbirthday = new SimpleDateFormat("yyyy-MM-dd").format(processDate);
-        
-        
-        if(mapper.getEmailCheck(e_mail)==0) {
-        	MemberDto mdto=new MemberDto();
-        	mdto.setId(e_mail);
-        	mdto.setName(name);
-        	mdto.setBirth(realbirthday);
-        	mdto.setPhoto(photo);
-        	mdto.setE_mail(e_mail);
-        	mapper.insertMember(mdto);
-        } 
-        
-        session.setAttribute("myid", e_mail);
-        session.setAttribute("kakaoName", name);
-        session.setAttribute("loginok", "yes");
-        session.setAttribute("mode", "guest");
-        session.setAttribute("kakaologin", "yes");
-        session.setAttribute("profile", photo);
-        session.setAttribute("access_Token", access_Token);
-        
-        //클라이언트의 이메일이 존재할 때 세션에 해당 이메일과 토큰 등록
-//        if (userInfo.get("email") != null) {
-//            session.setAttribute("userId", userInfo.get("email"));
-//            session.setAttribute("access_Token", access_Token);
-//        }
-        
-        return "redirect:/";
-		
+		HashMap<String, Object> userInfo = kakao.getUserInfo(access_Token);
+		System.out.println("login Controller : " + userInfo);
+
+		String name = (String) userInfo.get("nickname");
+		String birthday = (String) userInfo.get("birthday");
+		String photo = (String) userInfo.get("profile_image");
+		String e_mail = (String) userInfo.get("email");
+
+		Calendar date = Calendar.getInstance();
+		int nowyear = date.get(Calendar.YEAR);
+
+		String birth = Integer.toString(nowyear) + birthday;
+
+		// 현재 날짜의 타입
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+
+		// Date로 파싱
+		Date processDate = dateFormat.parse(birth);
+
+		// 변경할 타입으로의 형 변환
+		String realbirthday = new SimpleDateFormat("yyyy-MM-dd").format(processDate);
+
+		if (mapper.getEmailCheck(e_mail) == 0) {
+			MemberDto mdto = new MemberDto();
+			mdto.setId(e_mail);
+			mdto.setName(name);
+			mdto.setBirth(realbirthday);
+			mdto.setPhoto(photo);
+			mdto.setE_mail(e_mail);
+			mapper.insertMember(mdto);
+		}
+
+		session.setAttribute("myid", e_mail);
+		session.setAttribute("kakaoName", name);
+		session.setAttribute("loginok", "yes");
+		session.setAttribute("mode", "guest");
+		session.setAttribute("kakaologin", "yes");
+		session.setAttribute("profile", photo);
+		session.setAttribute("access_Token", access_Token);
+
+		return "redirect:/";
 	}
-	
+
 	@RequestMapping("/kakaologout")
 	public String kakaologout(HttpSession session) {
-	    if(session.getAttribute("kakaologin")==null) {
-	    	session.removeAttribute("myid");
-	    	session.removeAttribute("loginok");
-	    	session.removeAttribute("mode");
-	    	return "/member/loginForm";
-	    }
-	    
-	    kakao.kakaoLogout((String)session.getAttribute("access_Token"));
-	    session.removeAttribute("access_Token");
-	    session.removeAttribute("userId");
-	    session.removeAttribute("kakaologin");
-	    session.removeAttribute("kakaoName");
-	    session.removeAttribute("profile");
-	    session.removeAttribute("mode");
-	    session.removeAttribute("loginok");
-	    
-	    return "redirect:/";
+		if (session.getAttribute("kakaologin") == null) {
+			session.removeAttribute("myid");
+			session.removeAttribute("loginok");
+			session.removeAttribute("mode");
+			return "/member/loginForm";
+		}
+
+		kakao.kakaoLogout((String) session.getAttribute("access_Token"));
+		session.removeAttribute("access_Token");
+		session.removeAttribute("userId");
+		session.removeAttribute("kakaologin");
+		session.removeAttribute("kakaoName");
+		session.removeAttribute("profile");
+		session.removeAttribute("mode");
+		session.removeAttribute("loginok");
+
+		return "redirect:/";
 	}
-	
-	//아이디 찾기
+
+	// 아이디 찾기
 	@GetMapping("/findId")
-	public String findIdForm() throws Exception{
-		
+	public String findIdForm() throws Exception {
+
 		return "/member/findIdForm";
 	}
-	
+
 	@PostMapping("/findIdprocess")
-	public @ResponseBody HashMap findId(@RequestParam(value="name", required=false) String inputName, @RequestParam(value="hp", required=false) String inputHp){
+	public @ResponseBody HashMap<String, String> findId(@RequestParam(value = "name", required = false) String inputName,
+			@RequestParam(value = "hp", required = false) String inputHp) {
 		String result = service.findId(inputName, inputHp);
-		
-		HashMap<String, String> m = new HashMap<String,String>();
+
+		HashMap<String, String> m = new HashMap<String, String>();
 		m.put("id", result);
-		
+
 		return m;
 	}
-	
-	
-	// 비밀번호 찾기
-	 @GetMapping("/findPw")
-	 public String findPwGET() throws Exception{
-		 return "/member/findPwForm";
-	 }
 
-//	 @PostMapping("/findPw")
-//	 @ResponseBody
-//	 public MemberDto findPwPOST(@ModelAttribute MemberDto mdto) throws Exception{
-//	 	return service.checkIdEmail(mdto);
-//	 }
-	 @PostMapping("/findPw")
-	 @ResponseBody
-	 public String findPwPOST(@ModelAttribute MemberDto mdto) throws Exception{
-		 MemberDto dto = service.checkIdEmail(mdto);
-		 
-		 if(dto == null) {
-			 return "일치하는 회원정보가 없습니다.";
-		 } else {
-			 return "임시 비밀번호를 메일로 발송했습니다.";
-		 }
-	 	
-	 }
-	 
-		/*
-		 * @PostMapping("/updatePw") public void updatePw(String id, String e_mail)
-		 * throws Exception{ service.updatePw(id, e_mail); }
-		 */
-	
+	// 비밀번호 찾기
+	@GetMapping("/findPw")
+	public String findPwGET() throws Exception {
+		return "/member/findPwForm";
+	}
+
+	@PostMapping("/findPw")
+	@ResponseBody
+	public String findPwPOST(@ModelAttribute MemberDto mdto) throws Exception {
+		MemberDto dto = service.checkIdEmail(mdto);
+
+		if (dto == null) {
+			return "일치하는 회원정보가 없습니다.";
+		} else {
+			return "임시 비밀번호를 메일로 발송했습니다.";
+		}
+	}
+
 }
--- src/main/java/stay/data/controller/MypageController.java
+++ src/main/java/stay/data/controller/MypageController.java
@@ -22,186 +22,51 @@ import stay.data.service.MemberService;
 @Controller
 @RequestMapping("/mypage")
 public class MypageController {
-	
 	@Autowired
 	MemberMapper mapper;
-	
+
 	@Autowired
 	MemberService memberService;
 
 	@GetMapping("/mypageform")
 	public ModelAndView member1(HttpSession session) {
 		ModelAndView mview = new ModelAndView();
-		
-		String myid = (String)session.getAttribute("myid");
-		
+
+		String myid = (String) session.getAttribute("myid");
+
 		MemberDto memberDto = memberService.getMember(myid);
-		
+
 		memberDto.setId(myid);
-		
+
 		mview.addObject("memberDto", memberDto);
-		
+
 		mview.setViewName("/member/mypageForm");
-		
+
 		return mview;
 	}
-	
-	/*
+
 	@PostMapping("/update")
-	public String memberUpdate(@ModelAttribute MemberDto dto,
-			@RequestParam String id,
-			HttpSession session,
-			@RequestParam ArrayList<MultipartFile> upload) {
-
-		String loginok = (String) session.getAttribute("loginok");
-		if (loginok == null) {
-			return "/member/login";
-		}
-
+	public String memberUpdate(@ModelAttribute MemberDto dto, @RequestParam MultipartFile upload, HttpSession session) {
 		// 업로드할 폴더 지정
 		String path = session.getServletContext().getRealPath("/photo/memberPhoto");
 		System.out.println(path);
-		String photo = "";
 
-		if (upload.get(0).getOriginalFilename().equals("")) {
-			photo = null;
-		} else {
-			String photos = memberService.getMember(id).getPhoto();
-			String photoArray[] = photos.split(",");
-
-			for (int i = 0; i < photoArray.length; i++) {
-				File file = new File(path + "/" + photoArray[i]);
-				file.delete();
-			}
-
-			for (MultipartFile f : upload) {
-				String fName = f.getOriginalFilename();
-				photo += fName + ",";
-
-				// 업로드
-				try {
-					f.transferTo(new File(path + "\\" + fName));
-				} catch (IllegalStateException | IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-
-			photo = photo.substring(0, photo.length() - 1);
-		}
-
-		// 엔터키 입력
-		dto.setPhoto(photo);
-
-		memberService.updateMember(dto);
-		// update 호출
-		mapper.updateMember(dto);
-
-		// 메인으로
-		return "redirect:/mypage/mypageform";
-		
-	
-		
-		
-		 * String path = session.getServletContext().getRealPath("/photo/memberPhoto");
-		 * System.out.println(path); if(upload.get(0).getOriginalFilename().equals(""))
-		 * { photo = null; } else { String photo =
-		 * memberService.getMember(id).getPhoto(); dto.setPhoto(photo);
-		 * 
-		 * try { f.transferTo(new File(path + "\\" + fName)); } catch
-		 * (IllegalStateException | IOException e) { // TODO Auto-generated catch block
-		 * e.printStackTrace(); } } service.updateBoard(dto); return
-		 * "redirect:content?num="+dto.getNum()+"&currentPage="+currentPage;
-		 
-	}*/
-	
-	@PostMapping("/update")
-	public String memberUpdate(
-			@ModelAttribute MemberDto dto,
-			@RequestParam MultipartFile upload,
-			HttpSession session) 
-	{
-		
-		//만료됐을 수도 있으므로 다시 loginok 얻음
-		String loginok = (String) session.getAttribute("loginok");
-		String myid = (String)session.getAttribute("myid");
-		
-		//업로드할 폴더 지정
-		String path = session.getServletContext().getRealPath("/photo/memberPhoto");
-		System.out.println(path);
-		
-		if(upload.getOriginalFilename().equals("")) {
+		if (upload.getOriginalFilename().equals("")) {
 			dto.setPhoto(null);
 		} else {
 			String fname = upload.getOriginalFilename();
 			dto.setPhoto(fname);
-			
+
 			try {
-				upload.transferTo(new File(path+"\\"+ fname));
+				upload.transferTo(new File(path + "\\" + fname));
 			} catch (IllegalStateException | IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		
+
 		memberService.updateMember(dto);
 
 		// 메인으로
 		return "redirect:/mypage/mypageform";
 	}
-
-
-	
-	// updatepassform
-	/*
-	 * @GetMapping("/updatepassform") public String updatePassForm(@RequestParam
-	 * String id, Model model) { model.addAttribute("id",id); return
-	 * "/member/updatepassform"; }
-	 */
-
-	// 비밀번호 체크 후 updateform or passfail
-	/*
-	 * @PostMapping("/updatepass") public String updatePass(@RequestParam String
-	 * id, @RequestParam String pass) { // db로부터 비번맞나 체크 HashMap<String, String> map
-	 * = new HashMap<String, String>(); map.put("id", id); map.put("pass", pass);
-	 * 
-	 * int check = mapper.getCheckPass(map);
-	 * 
-	 * if (check == 1) { // 1:비번이 맞는경우 return "redirect:updateform?id=" + id; // id에
-	 * 해당하는 dto가져와야 하므로 } else { // 비번 틀린경우 return "/member/passfail"; } }
-	 */
-	
-	/*
-	 * @GetMapping("/updateform") public ModelAndView updateForm(@RequestParam
-	 * String id) { ModelAndView mview=new ModelAndView();
-	 * 
-	 * //db로부터 id에 해당하는 dto열기 MemberDto dto=mapper.getMember(id);
-	 * 
-	 * dto.setE_mail(dto.getE_mail());
-	 * 
-	 * mview.addObject("dto", dto);
-	 * 
-	 * mview.setViewName("/member/updateform");
-	 * 
-	 * return mview; }
-	 */
-	
-	//update
-	/*
-	 * @PostMapping("/update") public String memberUpdate(@ModelAttribute MemberDto
-	 * dto) {
-	 * 
-	 * //email 형식으로 넣어주기 dto.setE_mail(dto.getE_mail());
-	 * 
-	 * //update 호출 mapper.updateMember(dto);
-	 * 
-	 * //profile로 return "redirect:profile"; }
-	 */
-	/*
-	 * @GetMapping("/insertform") public String photoInsertForm() { return
-	 * "/member/photoInsertForm"; }
-	 */
-	
-	
-	
 }
